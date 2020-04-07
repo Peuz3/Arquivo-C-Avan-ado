a8fe8654ab7d2db243c75bc8e3e73a0355c0dfb7
Joao Pedro <jp.ssilva@live.com>
2020-04-07 18:20:18 -0300

    Serealizar JSON

---
+++ 03_SerealizarJSON/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using _00_Biblioteca;
+using System.IO;
+
+namespace _03_SerealizarJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario usuario = new Usuario() { Nome = "Erica Barbosa", CPF = "333.333.333-33", Email = "erica@gmail.com" };
+
+            JavaScriptSerializer serealizador = new JavaScriptSerializer();
+            string stringObjSerealizado = serealizador.Serialize(usuario);
+
+            StreamWriter streamWriter = new StreamWriter(@"C:\Users\jpssi\Documents\Udemy\Arquivo C# Avançado\02_SerealizadorJSON.json");
+            streamWriter.WriteLine(stringObjSerealizado);
+            streamWriter.Close();
+
+            Console.Write("Transação Efetuada!");
+            Console.ReadLine();
+
+
+        }
+    }
+}
