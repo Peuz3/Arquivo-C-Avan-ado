01fc75d3d8d75e83052c4ea772d4e72a8f0b5fc6
Joao Pedro <jp.ssilva@live.com>
2020-04-07 17:29:54 -0300

    Serializar

---
+++ 00_Biblioteca/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//É um projeto auxiliar que não executa nada, vai integrar outros projetos
+namespace _00_Biblioteca
+{
+    public class Usuario
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+
+    }
+}
---
+++ 01_SerializarXML/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using _00_Biblioteca;
+using System.Xml.Serialization;
+
+namespace _01_SerializarXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Criando meu objeto da biblioteca
+            Usuario usuario = new Usuario() { Nome = "João Pedro", CPF = "222.222.222-22", Email = "joao@gmail.com" };
+
+            //Salvar o objeto em um arquivo
+
+            XmlSerializer serializador = new XmlSerializer(typeof(Usuario));
+
+            StreamWriter streamWriter = new StreamWriter(@"C:\Users\jpssi\Documents\Udemy\Arquivo C# Avançado\01_SerializadorXML.xml");
+
+            //Método resposavel por fazer a serialização
+            serializador.Serialize(streamWriter, usuario);
+        }
+    }
+}
