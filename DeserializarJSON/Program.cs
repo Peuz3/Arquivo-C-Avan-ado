fed51e4fa5cc1d3ad32d33a20b8feff5540b0b35
Joao Pedro <jp.ssilva@live.com>
2020-04-07 18:37:14 -0300

    Deserializar JSON

---
+++ DeserializarJSON/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using _00_Biblioteca;
+using System.Web.Script.Serialization;
+
+namespace DeserializarJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader streamReader = new StreamReader(@"C:\Users\jpssi\Documents\Udemy\Arquivo C# Avançado\02_SerealizadorJSON.json");
+            string linhasDoArquivo = streamReader.ReadToEnd();
+
+            JavaScriptSerializer deserializador = new JavaScriptSerializer();
+            Usuario usuario = (Usuario)deserializador.Deserialize(linhasDoArquivo, typeof(Usuario));
+
+            Console.WriteLine("Usuário(Nome):{0}, CPF: {1} e Email: {2}", usuario.Nome, usuario.CPF, usuario.Email);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
